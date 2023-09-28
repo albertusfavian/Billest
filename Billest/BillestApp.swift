5d1245c6fc745444ce851064941cbbf2c803b3c6
Albertus Favian <favianalbert@gmail.com>
2023-09-28 13:01:01 +0700

    Initial Commit

---
+++ Billest/BillestApp.swift
@@ -0,0 +1,20 @@
+//
+//  BillestApp.swift
+//  Billest
+//
+//  Created by Mac-albert on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BillestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
